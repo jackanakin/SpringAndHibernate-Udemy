6a137de80de34d0524c9a1dc36e097262e6eca10
Jardel Kuhn <jardelkuhn@gmail.com>
2022-07-15 15:53:24 -0700

    basic MVC CRUD WebApp with Hibernate and Spring

---
+++ 10-SpringMVC-WebApp/README.md
@@ -0,0 +1,3 @@
+** Missing libs in WebContent/WEB-INF/lib/*
+
+## Simple WEB MVC App with basic CRUD operations (Hibernate, Spring)
---
+++ 10-SpringMVC-WebApp/src/com/example/demo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import com.example.demo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
